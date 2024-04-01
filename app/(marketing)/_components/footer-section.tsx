e85112e4fe67f9d340acd7958ca9fa31feafba65
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-02 00:08:14 +0530

    Update footer-section.tsx

--- app/(marketing)/_components/footer-section.tsx
+++ app/(marketing)/_components/footer-section.tsx
@@ -1,12 +1,4 @@
-import { Facebook, Instagram, Linkedin, Youtube } from "lucide-react";
 import Link from "next/link";
-
-export const Footer = ({ currentPage }: { currentPage?: string }) => {
-  const instagram = process.env.INSTAGRAM_PAGE || "#";
-  const facebook = process.env.FACEBOOK_PAGE || "#";
-  const youtube = process.env.YOUTUBE_CHANNEL || "#";
-  const linkedIn = process.env.LINKEDIN_PAGE || "#";
-
   return (
     <footer className="m-4 mt-8">
       <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 sm:flex-row sm:justify-center text-xs font-semibold text-gray-500">
